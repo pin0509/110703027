ca4e968be26181fa21e38e7bdbb4e22131f34e87
pin0509 <102577486+pin0509@users.noreply.github.com>
2022-03-29 15:14:07 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1648537193,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/moue-sheet0.png",
+		"images/moue-sheet1.png",
+		"images/stick-sheet0.png",
+		"media/lasersmall_000.m4a",
+		"media/lasersmall_000.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
